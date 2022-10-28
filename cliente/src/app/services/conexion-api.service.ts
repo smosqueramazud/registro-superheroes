dd4ea92d72cd6994994c26acce6a797102b78b9f
Sebastian Mosquera <sebastian.mosquera@supergiros.com.co>
2022-10-27 22:57:17 -0500

    se crean los servicios que llaman el api y se crean las interfaces que simulan los obj

--- cliente/src/app/services/conexion-api.service.ts
+++ cliente/src/app/services/conexion-api.service.ts
@@ -9,8 +9,58 @@ export class ConexionApiService {
   constructor(private http: HttpClient,
     ) { }
 
+    //servicio que trae todos los superheroes
     getSuperheroes(){
       let url = '/api';
       return this.http.get(url);
     }
+
+    //servicio que trae un superheroe por su nombre
+    getSuperheroePorNombre(nombre: string){
+      let url = '/api/nombre/';
+      return this.http.get(url + nombre);
+    }
+
+    //servicio que trae un superheroe por su ciudad
+    getSuperheroePorCiudad(ciudad: string){
+      let url = '/api/ciudad/';
+      return this.http.get(url + ciudad);
+    }
+
+    //servicio para agregar un superheroe
+    agregarSuperheroe(obj: Superheroe){
+      let url = '/api';
+      return this.http.post(url,obj);
+    }
+
+    //servicio para agregar un superheroe
+    eliminarSuperheroe(id:string){
+      let url = '/api/';
+      return this.http.delete(url + id);
+    }
+
+    //servicio para agregar un superheroe
+    editarSuperheroe(id:string, obj:SuperheroeActualizar){
+      let url = '/api/';
+      return this.http.put(url + id, obj);
+    }
+    
+}
+
+export interface Superheroe{
+  id_superheroe?:string;
+  nombre?: string;
+  grupo?: string;
+  ciudad_operacion?: string;
+  condicion?: string;
+  superpoder?: string;
+  vehiculo?: number;
+  tipo_vehiculo?: string;
+  logo: string;
+}
+
+export interface SuperheroeActualizar{
+  id_superheroe?:string;
+  nombre?: string;
+  ciudad_operacion?: string;
 }

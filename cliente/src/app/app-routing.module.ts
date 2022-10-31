57be7366589605e947ea21cfccfdf8fcddc345f7
Sebastian Mosquera <sebastian.mosquera@supergiros.com.co>
2022-10-31 14:08:45 -0500

    se agrega la vista grafica y su ruta

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgregarComponent } from './components/agregar/agregar.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ModificarComponent } from './components/modificar/modificar.component';
+import { VistaCardsComponent } from './vista-cards/vista-cards.component';
+
+
 
 const routes: Routes = [
 
   {path: '', redirectTo: '/inicio', pathMatch: 'full'},
   {path: 'inicio', component: InicioComponent},
   {path: 'agregar', component: AgregarComponent},
-  {path: 'modificar/:id', component: ModificarComponent}
+  {path: 'modificar/:id', component: ModificarComponent},
+  {path: 'vista-cards', component: VistaCardsComponent},
 
 
 ];
--- cliente/src/app/vista-cards/vista-cards.component.ts
+++ cliente/src/app/vista-cards/vista-cards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConexionApiService, Superheroe } from 'src/app/services/conexion-api.service';
+import { NgxSpinnerService } from "ngx-spinner";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vista-cards',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaCardsComponent implements OnInit {
 
-  constructor() { }
+  //ce crea la lista de superheroes con la interface Superheore
+  listaSuperheroes: Superheroe[];
+
+  constructor(private conexionApi: ConexionApiService,
+    private spinner: NgxSpinnerService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.listaSuperheroes = window.history.state.array;
+    if(this.listaSuperheroes === undefined){
+      this.obtenerListaSuperheroes();
+    }
+
+
+
+  let arrayOrdenado = this.listaSuperheroes.sort( (a, b) => {
+      if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
+        return -1;
+      }
+      if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    });
+    console.log(arrayOrdenado); 
+    
   }
 
+
+  obtenerListaSuperheroes(){
+    this.spinner.show();
+    this.conexionApi.getSuperheroes().subscribe(
+      res => {
+        this.spinner.hide();
+        this.listaSuperheroes = <any>res; //se debe dejar tipo any ya que listaSuperheroes es tipo Superheroe
+      },
+      err => {
+        this.spinner.hide();
+        console.log(err)
+        alert(`Ha ocurrido un error consultando la lista de superhéroes, por favor intenta de nuevo mas tarde`)
+      }
+    )
+  }
+
+  eliminarSuperheroe(id:string, nombre:string){
+    this.conexionApi.eliminarSuperheroe(id).subscribe(
+      res => {
+        alert(`El superhéroe ${nombre} se elimino correctamente`);
+        this.obtenerListaSuperheroes();
+      },
+      err => {
+        console.log(err)
+        alert(`Ha ocurrido un error eliminando el superhéroe, por favor intenta de nuevo mas tarde`)
+      }
+    )
+  }
+
+  modificarSuperheroe(id:string){
+    this.router.navigate(['/modificar/' + id]); //se manda el id por url
+  }
 }
